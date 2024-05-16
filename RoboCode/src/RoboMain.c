b70deab9b797b5ed9a7647a3a82f9eddd0cffc99
Caden Grace Jacobs <gxjacobs@ucsc.edu>
2024-05-15 22:40:21 -0700

    Updated ROBO.c with new motor connections and RoboMain.c tape sensors

--- RoboCode/src/RoboMain.c
+++ RoboCode/src/RoboMain.c
@@ -10,23 +10,54 @@
 #include "ROBO.h"
 #include "pwm.h"
 #include "AD.h"
+#include <inttypes.h>
 //#include "ES_Timers.h"
 //#include "ES_Configure.h"
 #include "BOARD.h"
 #include <stdio.h>
+#include "LED.h"
+
 
 int main(void) {
     ROBO_Init();
     BOARD_Init();
     AD_Init();
+    LED_Init();
     PWM_Init();
-    PWM_AddPins(PWM_PORTY10);
+    PWM_AddPins(PWM_PORTY12);
     PWM_AddPins(PWM_PORTY04);
+    
+    LED_AddBanks(LED_BANK1);
+    
+    AD_AddPins(AD_PORTW6);
+    AD_AddPins(AD_PORTW4);
+    AD_AddPins(AD_PORTW3);
+    AD_AddPins(AD_PORTW5);
     //int time = ES_Timer_GetTime();
     int state = 0;
     while(1){
-            RoboLeftMtrSpeed(100);
-            RoboRightMtrSpeed(100);
+       uint16_t topRight=  AD_ReadADPin(AD_PORTW6);
+       uint16_t right=  AD_ReadADPin(AD_PORTW4);
+       uint16_t topLeft=  AD_ReadADPin(AD_PORTW3);
+       uint16_t left=  AD_ReadADPin(AD_PORTW5);
+       
+       if (topRight < 20) {
+           LED_SetBank(LED_BANK1, 0x1);
+       } else if (right < 20){
+           LED_SetBank(LED_BANK1, 0x2);
+       } else if (topLeft < 20) {
+           LED_SetBank(LED_BANK1, 0x4);
+       } else if (left < 20) {
+           LED_SetBank(LED_BANK1, 0x8);
+       } else {
+           LED_SetBank(LED_BANK1, 0x0);
+       }
+       //printf("TopRight: %d\r\n", topRight);
+       //printf("Right: %d\r\n", right);
+       //printf("TopLeft: %d\r\n", topLeft);
+       //printf("Left: %d\r\n", left);
+//            RoboLeftMtrSpeed(95);
+//            RoboRightMtrSpeed(100);
     }
     return 0;
 }
