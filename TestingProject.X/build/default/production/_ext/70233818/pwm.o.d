fc79052fa995d2a91aa2ec3ae3cee3e4d8316511
Caden Grace Jacobs <gxjacobs@ucsc.edu>
2024-05-28 18:08:47 -0700

    More clean up

--- TestingProject.X/build/default/production/_ext/70233818/pwm.o.d
+++
@@ -1,8 +0,0 @@
-build/default/production/_ext/70233818/pwm.o: \
- C:/Users/jeemcast/OneDrive\ -\ ucsc.edu/ECE118/src/pwm.c \
- C:/Users/jeemcast/OneDrive\ -\ ucsc.edu/ECE118/include/BOARD.h \
- C:/Users/jeemcast/OneDrive\ -\ ucsc.edu/ECE118/include/pwm.h
-
-C:/Users/jeemcast/OneDrive\ -\ ucsc.edu/ECE118/include/BOARD.h:
-
-C:/Users/jeemcast/OneDrive\ -\ ucsc.edu/ECE118/include/pwm.h:
--- TestingProject.X/build/default/production/_ext/70233818/serial.o.d
+++
@@ -1,8 +0,0 @@
-build/default/production/_ext/70233818/serial.o: \
- C:/Users/jeemcast/OneDrive\ -\ ucsc.edu/ECE118/src/serial.c \
- C:/Users/jeemcast/OneDrive\ -\ ucsc.edu/ECE118/include/serial.h \
- C:/Users/jeemcast/OneDrive\ -\ ucsc.edu/ECE118/include/BOARD.h
-
-C:/Users/jeemcast/OneDrive\ -\ ucsc.edu/ECE118/include/serial.h:
-
-C:/Users/jeemcast/OneDrive\ -\ ucsc.edu/ECE118/include/BOARD.h:
