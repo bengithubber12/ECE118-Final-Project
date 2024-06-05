6a33286daa784459c608efb39e454382cf2bc574
gxjacobs <166069663+gxjacobs@users.noreply.github.com>
2024-06-05 07:06:19 -0700

    updated bumper service

--- FinalProjectV2.X/BumperService.c
+++ FinalProjectV2.X/BumperService.c
@@ -72,78 +72,7 @@ ES_Event RunBumperService(ES_Event ThisEvent) {
             break;
         case ES_TIMEOUT:
             ES_Timer_InitTimer(BUMPER_SERVICE_TIMER, BUMPER_TIMER_TICKS); // runs every 5ms
-            curEvent = ~((PORTX08_BIT << 7) | ((PORTX06_BIT << 6) | ((PORTX05_BIT << 5) | ((PORTX12_BIT << 4) | ((PORTX11_BIT << 3) | ((PORTX04_BIT << 2) | ((PORTX03_BIT << 1) | PORTX10_BIT)))))));
-            //printf("BumperRead: %x\r\n", bumperRead);
-            //Front Left Bumper
-            /*
-            if (bumperRead & BOTTOM_FLEFT_BUMP_MASK) {
-                //printf("FLB Triggered.\r\n");
-                curEvent &= ~(1 << 0);
-            }else {
-                curEvent |= (1 << 0);
-            }
-            
-            //Front Right Bumper
-            if (bumperRead & BOTTOM_FRIGHT_BUMP_MASK) {
-                //printf("FRB Triggered.\r\n");
-                curEvent &= ~(1 << 1); //2
-            } else {
-                curEvent |= (1 << 1);
-            }
-            
-            //Back Right Bumper 
-            if (bumperRead & BOTTOM_BRIGHT_BUMP_MASK) {
-                //printf("BRB Triggered.\r\n");
-                curEvent &= ~(1 << 2); //4 
-            } else {
-                curEvent |= (1 << 2);
-            }
-            
-            //Back Left Bumper
-            if (bumperRead & BOTTOM_BLEFT_BUMP_MASK) {
-                //printf("BLB Triggered.\r\n");
-                curEvent &= ~(1 << 3);  //8
-            } else {
-                curEvent |= (1 << 3);
-            }
-            
-            if (bumperRead & TOP_FLEFT_BUMP_MASK) {
-                //printf("FLB Triggered.\r\n");
-                curEvent &= ~(1 << 4); //10
-            }else {
-                curEvent |= (1 << 4);
-            }
-            
-            if (bumperRead & TOP_FRIGHT_BUMP_MASK) {
-                //printf("FRB Triggered.\r\n");
-                curEvent &= ~(1 << 5); //20
-            } else {
-                curEvent |= (1 << 5);
-            }
-             
-            if (bumperRead & TOP_BRIGHT_BUMP_MASK) {
-                //printf("BRB Triggered.\r\n");
-                curEvent &= ~(1 << 6); //40
-            } else {
-                curEvent |= (1 << 6);
-            }
-
-            if (bumperRead & TOP_BLEFT_BUMP_MASK) {
-                //printf("BLB Triggered.\r\n");
-                curEvent &= ~(1 << 7);  //80
-            } else {
-                curEvent |= (1 << 7);
-            }
-             * */
-            /*
-            else if (bumperRead & FRONT_BUMPERS) {
-                //printf("Front bumpers Triggered\r\n");
-                curEvent = FLB_EVENT;
-            }
-            else {
-                curEvent = NO_BUMPER_EVENT;
-            }
-            */
+            curEvent = ~((PORTX08_BIT << 7) | ((PORTX06_BIT << 6) | ((PORTX05_BIT << 5) | ((PORTX12_BIT << 4) | ((PORTX11_BIT << 3) | ((PORTX04_BIT << 2) | ((PORTX03_BIT << 1) | ((PORTX10_BIT)))))))));
             // compare previous and current values
             if (lastEvent != curEvent){
                 ReturnEvent.EventType = BUMPER_STATUS_CHANGE;
